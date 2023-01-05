dd43114b97922da72cc85c06ff057318c98e9d16
mahaque <haquema@outlook.com>
2023-01-05 18:45:40 +0000

    complete challenge part 2

---
+++ new-project/searchcandies.test.js
@@ -0,0 +1,23 @@
+const searchCandies = require("./searchcandies");
+
+describe("searchCandies", () => {
+  it("returns array of all candies beginning with Ma and price less than 10", () => {
+    expect(searchCandies("Ma", 10)).toEqual(["Mars", "Maltesers"]);
+  });
+
+  it("returns array of all candies beginning with Ma and price less than 2", () => {
+    expect(searchCandies("Ma", 2)).toEqual(["Mars"]);
+  });
+
+  it("returns array of all candies beginning with S and price less than 10", () => {
+    expect(searchCandies("S", 10)).toEqual([ 'Skitties', 'Skittles', 'Starburst' ]);
+  });
+
+  it("returns array of all candies beginning with S and price less than 4", () => {
+    expect(searchCandies("S", 4)).toEqual([ 'Skitties', 'Skittles' ]);
+  });
+
+  it("returns array of all candies beginning with s and price less than 4", () => {
+    expect(searchCandies("s", 4)).toEqual([ 'Skitties', 'Skittles' ]);
+  });
+})
